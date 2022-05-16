3c76bcfe64cf3ea88fdef3ea10efe7915ff1e812
Immanuel Daviel Alexander Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-05-16 09:21:54 -0600

    Update announcements.js

--- js/announcements.js
+++ js/announcements.js
@@ -1,6 +1,21 @@
-getHTML("https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/announcement.html", "ann_container");
-setTimeout(()=>{
-    if(announcement_type.innerText == "DEFAULT"){
-        getHTML("https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/placeholder.html", "ann_container");
-    }
-}, 300)
+getHTML(
+   "https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/announcement.html",
+   "ann_container"
+);
+ann_container.style.opacity = 0;
+setTimeout(() => {
+   try {
+      if (announcement_type.innerText == "DEFAULT") {
+         getHTML(
+            "https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/placeholder.html",
+            "ann_container"
+         );
+         ann_container.style.opacity = 1;
+      }
+   } catch (error) {
+      getHTML(
+         "https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/announcement.html",
+         "ann_container"
+      );
+   }
+}, 350);
