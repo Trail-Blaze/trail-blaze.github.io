bcae6b0d058d426e30336f4a13228447de71df89
Immanuel Daviel Alexander Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-05-16 09:31:27 -0600

    Update announcements.js

--- js/announcements.js
+++ js/announcements.js
@@ -2,8 +2,8 @@ getHTML(
    "https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/announcement.html",
    "ann_container"
 );
-ann_container.style.opacity = 0;
 setTimeout(() => {
+   ann_container.style.opacity = 0;
    try {
       if (announcement_type.innerText == "DEFAULT") {
          getHTML(
