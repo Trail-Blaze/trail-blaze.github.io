05e1082ff6e635caa5485a371535108a41931f2a
Immanuel Daviel Alexander Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-05-16 09:36:12 -0600

    Update announcements.js

--- js/announcements.js
+++ js/announcements.js
@@ -2,20 +2,23 @@ getHTML(
    "https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/announcement.html",
    "ann_container"
 );
-setTimeout(() => {
-   ann_container.style.opacity = 0;
-   try {
-      if (announcement_type.innerText == "DEFAULT") {
-         getHTML(
-            "https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/placeholder.html",
-            "ann_container"
-         );
-      }
-   } catch (error) {
+
+try {
+   if (announcement_type.innerText == "DEFAULT") {
+      ann_container.style.opacity = 0;
       getHTML(
-         "https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/announcement.html",
+         "https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/placeholder.html",
          "ann_container"
       );
    }
+} catch (error) {
+   ann_container.style.opacity = 0;
+   getHTML(
+      "https://raw.githubusercontent.com/Trail-Blaze/marketplace/main/featuredContent/announcements/partials/announcement.html",
+      "ann_container"
+   );
+}
+
+setTimeout(() => {
+   ann_container.style.opacity = 1;
 }, 425);
-ann_container.style.opacity = 1;
